edf09673f9a631e7e7a45243a9eb9c5a21a43408
Karthik Nathan <koolkarthik97@gmail.com>
2018-09-20 11:52:00 +0530

    Create techStackContent.js

---
+++ techStackContent.js
@@ -0,0 +1,43 @@
+[
+  {
+    "name": "Client Layer",
+    "children": [
+      {
+        "name": "Development Languages",
+        "children": [
+          {
+            "name": "HTML5"
+          },
+          {
+            "name": "Require.js"
+          },
+          {
+            "name": "Backbone.js"
+          }
+        ]
+      },
+      {
+        "name": "Templating Languages",
+        "children": [
+          {
+            "name": "Handlebars"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Multi channel interaction Layer",
+    "children": [
+      {
+        "name": "REST"
+      },
+      {
+        "name": "JSON"
+      },
+      {
+        "name": "OAuth 2.0"
+      }
+    ]
+  }
+]
