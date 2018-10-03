a4f599166298a571081202043a680c59f3089bcb
Karthik Nathan <koolkarthik97@gmail.com>
2018-10-03 17:10:07 +0530

    Add files via upload

---
+++ coffee-map/XJJYqy.js
@@ -0,0 +1,206 @@
+[
+  {
+    "name": "Client Layer",
+    "children": [
+      {
+        "name": "Development Languages",
+        "children": [
+          {
+            "name": "HTML5"
+          },
+          {
+            "name": "CSS3"
+          },
+          {
+            "name": "Javascript"
+          }
+        ]
+      },
+      {
+        "name": "Templating Languages",
+        "children": [
+          {
+            "name": "Handlebars"
+          }
+        ]
+      },
+      {
+        "name": "Client-side MVC",
+        "children": [
+          {
+            "name": "Underscore.js"
+          },
+          {
+            "name": "Marionnette.js"
+          },
+          {
+            "name": "Backbone.js"
+          }
+        ]
+      },
+      {
+        "name": "Responsive Web Design",
+        "children": [
+          {
+            "name": "BootStrap"
+          }
+        ]
+      },
+      {
+        "name": "Dependency Management",
+        "children": [
+          {
+            "name": "Require.js"
+          }
+        ]
+      },
+      {
+        "name": "Build and Deployment",
+        "children": [
+          {
+            "name": "Grunt"
+          },
+          {
+            "name": "Apache Maven"
+          },
+          {
+            "name": "NGINX"
+          }
+        ]
+      },
+      {
+        "name": "UI Tooling",
+        "children": [
+          {
+            "name": "jQuery & jQuery Mobile"
+          },
+          {
+            "name": "Scaffolding"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Multi channel interaction Layer",
+    "children": [
+      {
+        "name": "Interoperability Standards",
+        "children": [
+          {
+            "name": "REST"
+          },
+          {
+            "name": "JSON"
+          },
+          {
+            "name": "OAuth 2.0"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Microservice Layer",
+    "children": [
+      {
+        "name": "Dev Platform / Framework",
+        "children": [
+          {
+            "name": "Java 8"
+          },
+          {
+            "name": "Spring"
+          }
+        ]
+      },
+      {
+        "name": "Application Containerization",
+        "children": [
+          {
+            "name": "Spring Boot"
+          },
+          {
+            "name": "Spring Boot Web"
+          },
+          {
+            "name": "Spring Cloud"
+          },
+          {
+            "name": "RxJava"
+          },
+          {
+            "name": "Apache Tomcat"
+          }
+        ]
+      },
+      {
+        "name": "Data",
+        "children": [
+          {
+            "name": "Pivotal Gemfire"
+          },
+          {
+            "name": "Spring Integration"
+          }
+        ]
+      },
+      {
+        "name": "Development-Test Tools",
+        "children": [
+          {
+            "name": "Spring Tool Suite"
+          },
+          {
+            "name": "JUnit"
+          },
+          {
+            "name": "HP UFT12"
+          },
+          {
+            "name": "Rest Assured"
+          },
+          {
+            "name": "Cucumber"
+          },
+          {
+            "name": "ZAPTEST"
+          },
+          {
+            "name": "CA LISA"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Cloud Framework",
+    "children": [
+      {
+        "name": "Runtime Platform",
+        "children": [
+          {
+            "name": "Pivotal Cloud Foundry"
+          },
+          {
+            "name": "Netflix Hystrix"
+          },
+          {
+            "name": "Netflix Ribbon"
+          }
+        ]
+      },
+      {
+        "name": "Infrastructure & Automation",
+        "children": [
+          {
+            "name": "VMWare"
+          },
+          {
+            "name": "BOSH"
+          }
+        ]
+      }
+    ]
+  }
+]
